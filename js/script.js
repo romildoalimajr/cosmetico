d3d122129b5bc1777b9d2de13d1317368f8b7d27
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2022-04-08 23:52:37 -0300

    finalização do site-modelo para loja de cosméticos

--- js/script.js
+++ js/script.js
@@ -46,14 +46,81 @@ var swiper = new Swiper(".products-slider", {
         0:{
             slidesPerView: 1,
         },
-        500:{
+        550:{
             slidesPerView: 2,
         },
-        768:{
+        850:{
             slidesPerView: 3,
         },
-        1024:{
+        1200:{
             slidesPerView: 4,
         },
-    }
+    },
+  });
+
+  var swiper = new Swiper(".arrivals-slider", {
+    loop: true,
+    grabeCursor: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        550:{
+            slidesPerView: 2,
+        },
+        850:{
+            slidesPerView: 3,
+        },
+        1200:{
+            slidesPerView: 4,
+        },
+    },
+  });
+
+  var swiper = new Swiper(".reviews-slider", {
+    loop: true,
+    grabeCursor: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+    },
+  });
+  
+  var swiper = new Swiper(".blogs-slider", {
+    loop: true,
+    grabeCursor: true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        650:{
+            slidesPerView: 2,
+        },
+        
+        1200:{
+            slidesPerView: 3,
+        },
+    },
   });
